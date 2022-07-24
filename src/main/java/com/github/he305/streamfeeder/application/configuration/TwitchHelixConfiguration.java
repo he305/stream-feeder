96c2135e8a752e4982ce7f8e65b1b46b19451e2e
sergey <he305@mail.ru>
2022-07-24 20:45:49 +0300

    Added logging

--- src/main/java/com/github/he305/streamfeeder/application/configuration/TwitchHelixConfiguration.java
+++ src/main/java/com/github/he305/streamfeeder/application/configuration/TwitchHelixConfiguration.java
@@ -2,11 +2,13 @@ package com.github.he305.streamfeeder.application.configuration;
 
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.helix.TwitchHelix;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class TwitchHelixConfiguration {
     @Value("${twitch-client-id-env:default}")
     private String clientId;
@@ -16,6 +18,8 @@ public class TwitchHelixConfiguration {
 
     @Bean
     public TwitchHelix getTwitchClient() {
+        log.info("Client id: " + clientId);
+        log.info("Client secret: " + clientSecret);
         return TwitchClientBuilder.builder()
                 .withEnableHelix(true)
                 .withClientId(clientId)
