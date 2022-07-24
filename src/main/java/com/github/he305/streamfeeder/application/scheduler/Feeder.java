126f7ef62d305ead1a86441bbbdbbcbbc9f45ea5
sergey <he305@mail.ru>
2022-07-24 18:31:23 +0300

    Working Feeder

--- src/main/java/com/github/he305/streamfeeder/application/scheduler/Feeder.java
+++ src/main/java/com/github/he305/streamfeeder/application/scheduler/Feeder.java
@@ -1,12 +1,18 @@
 package com.github.he305.streamfeeder.application.scheduler;
 
+import com.github.he305.streamfeeder.common.dto.StreamEnd;
 import com.github.he305.streamfeeder.common.entity.Channel;
-import com.github.he305.streamfeeder.common.service.ChannelService;
+import com.github.he305.streamfeeder.common.entity.StreamData;
+import com.github.he305.streamfeeder.common.entity.StreamExternalData;
+import com.github.he305.streamfeeder.common.factory.StreamExternalServiceFactory;
+import com.github.he305.streamfeeder.common.service.ProducerExchangeService;
+import com.github.he305.streamfeeder.common.service.StreamExternalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -14,11 +20,40 @@ import java.util.List;
 @Slf4j
 public class Feeder {
 
-    private final ChannelService service;
+    private final ProducerExchangeService service;
+    private final StreamExternalServiceFactory streamExternalServiceFactory;
+    private List<Channel> savedChannels;
 
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 60000)
     public void retrieveChannels() {
         List<Channel> channels = service.getChannels();
-        channels.stream().map(Channel::toString).forEach(log::info);
+        if (!channels.equals(savedChannels)) {
+            log.info("Save new channels");
+            savedChannels = channels;
+        }
+
+        savedChannels
+                .forEach(channel -> {
+                    StreamExternalService streamExternalService = streamExternalServiceFactory.getStreamExternalService(channel.getPlatform());
+                    StreamExternalData data = streamExternalService.getStreamExternalDataForNickname(channel.getNickname());
+                    log.info(data.toString());
+                    if (Boolean.TRUE.equals(data.getIsLive())) {
+                        StreamData toSend = new StreamData(
+                                data.getGameName(),
+                                data.getTitle(),
+                                data.getViewerCount(),
+                                LocalDateTime.now(),
+                                channel.getId()
+                        );
+                        boolean hasSent = service.addStreamData(toSend);
+                        log.info("Sent add? " + hasSent);
+                    } else {
+                        if (Boolean.TRUE.equals(channel.getIsLive())) {
+                            StreamEnd endRequest = new StreamEnd(LocalDateTime.now(), channel.getId());
+                            boolean hasSent = service.endStream(endRequest);
+                            log.info("Sent end? " + hasSent);
+                        }
+                    }
+                });
     }
 }
--- src/main/java/com/github/he305/streamfeeder/common/service/ChannelService.java
+++ src/main/java/com/github/he305/streamfeeder/common/service/ChannelService.java
@@ -5,5 +5,6 @@ import com.github.he305.streamfeeder.common.entity.Channel;
 import java.util.List;
 
 public interface ChannelService {
-    List<Channel> getChannels();
+    List<Channel> getAllChannels();
+    List<Channel> getLiveChannels();
 }
