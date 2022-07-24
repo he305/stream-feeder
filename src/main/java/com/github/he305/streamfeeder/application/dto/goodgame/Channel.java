fe357ea7608ed1611dc808cef8369094c4e5a160
sergey <he305@mail.ru>
2022-07-24 22:53:29 +0300

    Added goodgame dto

---
+++ src/main/java/com/github/he305/streamfeeder/application/dto/goodgame/Channel.java
@@ -0,0 +1,19 @@
+package com.github.he305.streamfeeder.application.dto.goodgame;
+
+public class Channel {
+    public String stream_id;
+    public String key;
+    public String premium;
+    public String title;
+    public String status;
+    public String viewers;
+    public boolean max_viewers;
+    public String usersinchat;
+    public String embed;
+    public String img;
+    public String thumb;
+    public String description;
+    public String adult;
+    public String games;
+    public String url;
+}
