a5c40e3a65463cd22c57e635af1e23b23e678932
sergey <he305@mail.ru>
2022-07-25 02:07:00 +0300

    Proper factory

--- src/main/java/com/github/he305/streamfeeder/application/factory/StreamExternalServiceFactoryImpl.java
+++ src/main/java/com/github/he305/streamfeeder/application/factory/StreamExternalServiceFactoryImpl.java
@@ -2,31 +2,30 @@ package com.github.he305.streamfeeder.application.factory;
 
 import com.github.he305.streamfeeder.common.entity.Platform;
 import com.github.he305.streamfeeder.common.factory.StreamExternalServiceFactory;
-import com.github.he305.streamfeeder.common.service.GoodgameStreamExternalService;
 import com.github.he305.streamfeeder.common.service.StreamExternalService;
-import com.github.he305.streamfeeder.common.service.TwitchStreamExternalService;
-import com.github.he305.streamfeeder.common.service.WasdStreamExternalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class StreamExternalServiceFactoryImpl implements StreamExternalServiceFactory {
-    private final TwitchStreamExternalService twitchStreamExternalService;
-    private final GoodgameStreamExternalService goodgameStreamExternalService;
-    private final WasdStreamExternalService wasdStreamExternalService;
+    private static final Map<Platform, StreamExternalService> serviceMap = new EnumMap<>(Platform.class);
+    private final List<StreamExternalService> services;
+
+    @PostConstruct
+    public void initServiceMap() {
+        services.forEach(s -> serviceMap.put(s.getServiceType(), s));
+    }
 
     @Override
     public StreamExternalService getStreamExternalService(Platform platform) {
-        switch (platform) {
-            case TWITCH:
-                return twitchStreamExternalService;
-            case GOODGAME:
-                return goodgameStreamExternalService;
-            case WASD:
-                return wasdStreamExternalService;
-            default:
-                throw new UnsupportedOperationException();
-        }
+        StreamExternalService service = serviceMap.get(platform);
+        if (service == null) throw new UnsupportedOperationException();
+        return service;
     }
 }
