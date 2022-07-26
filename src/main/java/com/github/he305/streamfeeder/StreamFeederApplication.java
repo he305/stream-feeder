244eeeb0d9eaaf10d44e9871cf198860d280f62f
sergey <he305@mail.ru>
2022-07-26 18:21:20 +0300

    Adding scheduling conditional

--- src/main/java/com/github/he305/streamfeeder/StreamFeederApplication.java
+++ src/main/java/com/github/he305/streamfeeder/StreamFeederApplication.java
@@ -3,10 +3,8 @@ package com.github.he305.streamfeeder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class StreamFeederApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/github/he305/streamfeeder/application/factory/StreamExternalServiceFactoryImplTest.java
@@ -0,0 +1,48 @@
+package com.github.he305.streamfeeder.application.factory;
+
+import com.github.he305.streamfeeder.common.entity.Platform;
+import com.github.he305.streamfeeder.common.service.StreamExternalService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@TestPropertySource(properties = "app.scheduling.enable=false")
+class StreamExternalServiceFactoryImplTest {
+
+    @Autowired
+    private List<StreamExternalService> serviceList;
+    @Autowired
+    private StreamExternalServiceFactoryImpl underTest;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getStreamExternalService_success() {
+        serviceList.forEach(service -> {
+            StreamExternalService actual = underTest.getStreamExternalService(service.getServiceType());
+            assertEquals(service.getServiceType(), actual.getServiceType());
+        });
+    }
+
+    @Test
+    void getStreamExternalService_throwUnsupported() {
+        Platform platform = Mockito.mock(Platform.class);
+        Mockito.when(platform.ordinal()).thenReturn(Integer.MAX_VALUE);
+        assertThrows(UnsupportedOperationException.class, () ->
+                underTest.getStreamExternalService(platform));
+    }
+}
