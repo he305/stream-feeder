27a4784bee12380e9c6d1fa8fcb8f283ea73df08
sergey <he305@mail.ru>
2022-07-28 14:13:27 +0300

    Added general exception and mapping exception

---
+++ src/main/java/com/github/he305/streamfeeder/common/exception/StreamExternalServiceException.java
@@ -0,0 +1,7 @@
+package com.github.he305.streamfeeder.common.exception;
+
+public class StreamExternalServiceException extends RuntimeException {
+    public StreamExternalServiceException(String mes) {
+        super(mes);
+    }
+}
---
+++ src/main/java/com/github/he305/streamfeeder/common/exception/StreamExternalServiceMappingException.java
@@ -0,0 +1,7 @@
+package com.github.he305.streamfeeder.common.exception;
+
+public class StreamExternalServiceMappingException extends StreamExternalServiceException {
+    public StreamExternalServiceMappingException(String mes) {
+        super(mes);
+    }
+}
