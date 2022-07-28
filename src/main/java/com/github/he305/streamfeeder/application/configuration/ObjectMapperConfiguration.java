9aee7c336d8d4bb9e427d93f7b04e347111a6dc8
sergey <he305@mail.ru>
2022-07-28 14:11:11 +0300

    Added ObjectMapperConfiguration

---
+++ src/main/java/com/github/he305/streamfeeder/application/configuration/ObjectMapperConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.he305.streamfeeder.application.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfiguration {
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+}
