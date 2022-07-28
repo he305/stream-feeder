defe5b98be3f5288903e13b11bb214ef9de6a412
sergey <he305@mail.ru>
2022-07-28 17:35:41 +0300

    Added ProducerExchangeException

---
+++ src/main/java/com/github/he305/streamfeeder/common/exception/ProducerExchangeException.java
@@ -0,0 +1,7 @@
+package com.github.he305.streamfeeder.common.exception;
+
+public class ProducerExchangeException extends RuntimeException {
+    public ProducerExchangeException(String mes) {
+        super(mes);
+    }
+}
